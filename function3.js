3f7ff5c7305f1c5dd27c92998ef70bc64afbf816
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2024-01-16 03:58:28 +0100

    Update function3.js

--- function3.js
+++ function3.js
@@ -1,35 +1,22 @@
-window.function = async function (address,apikey) {
+window.function = async function (recherche,apikey) {
   var myHeaders = new Headers();
   let apikeyvalue = apikey.value;
 myHeaders.append("X-API-KEY", `${apikeyvalue}`);
 myHeaders.append("Content-Type", "application/json");
-  var raw = JSON.stringify([
-    {
-        "params": {
-            "q": {
-                "type": "number",
-                "value": 14
-            },
-            "recherche": {
-                "type": "string",
-                "value": "Butanol"
-            }
-        }
-    }
-]);
+
+
+var raw = JSON.stringify({
+  "q": recherche.value
+});
 
 var requestOptions = {
   method: 'POST',
+  headers: myHeaders,
   body: raw,
   redirect: 'follow'
 };
-  if (address.value === undefined) return undefined;
-  let number = address.value;
 
-   const response2 = await fetch
-  (`https://randomuser.me/api/?results=${number}`);
-
-  const response = await fetch(`https://hook.eu1.make.com/8v5egatxkxez2xthfj7qmq1h5nn4c7n7`, requestOptions);
+const response = await fetch("https://google.serper.dev/search", requestOptions);
   
   const data = await response.json();
   const jsonString = JSON.stringify(data); // Convert the object to a JSON string
