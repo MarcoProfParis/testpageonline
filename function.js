f0f84bf6bbe0f9ecac9df89625ddf3f72f524e72
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2024-04-09 18:09:12 +0200

    Update function.js

--- function.js
+++ function.js
@@ -1,24 +1,28 @@
 
 
 window.function = async function(APIKEY, ENDPOINT, QUERYSTRING) {
-if (QUERYSTRING.value === undefined) return "4En attente de QUERYSTRING";
-    const headers = {
-        'X-CMC_PRO_API_KEY': APIKEY.value,
-        'Accept': 'application/json'
-    };
-console.log("header ",headers);
+if (QUERYSTRING.value === undefined) return "9En attente de QUERYSTRING";
+var headers = new Headers();
+let apikeyvalue = APIKEY.value;
+headers.append("X-CMC_PRO_API_KEY", `${apikeyvalue}`);
+    headers.append("Accept", "application/json");
+
+console.log("header ",headers.value);
     // Constructing URL with query parameters
     const url = new URL(ENDPOINT.value);
 const QUERY = QUERYSTRING.value;
     if (QUERY) {
         Object.keys(QUERY.value).forEach(key => url.searchParams.append(key, QUERY[key]));
     }
+var requestOptions = {
+      method: 'GET',
+      headers: headers,
+      redirect: 'follow'
+    };
 
+console.log("requestOptions ",requestOptions);
     try {
-        const response = await fetch('https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?symbol=BTC', {
-            method: 'GET',
-            headers: headers
-        });
+        const response = await fetch('https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?symbol=BTC', requestOptions);
 console.log("response ",response);
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
