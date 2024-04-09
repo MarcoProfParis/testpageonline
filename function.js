e6706d9a8e489b644230818361ee37794605d7da
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2024-04-09 17:59:16 +0200

    Update function.js

--- function.js
+++ function.js
@@ -1,13 +1,7 @@
+
+
 window.function = async function(APIKEY, ENDPOINT, QUERYSTRING) {
-    if (QUERYSTRING.value === undefined) {
-        console.log("2Waiting for QUERYSTRING");
-        return "2Waiting for QUERYSTRING";
-    }
-
-    console.log("API Key:", APIKEY.value);
-    console.log("Endpoint:", ENDPOINT.value);
-    console.log("Query String:", QUERYSTRING.value);
-
+if (QUERYSTRING.value === undefined) return "En attente de QUERYSTRING";
     const headers = {
         'X-CMC_PRO_API_KEY': APIKEY.value,
         'Accept': 'application/json'
@@ -15,16 +9,12 @@ window.function = async function(APIKEY, ENDPOINT, QUERYSTRING) {
 
     // Constructing URL with query parameters
     const url = new URL(ENDPOINT.value);
-    const QUERY = QUERYSTRING.value;
+const QUERY = QUERYSTRING.value;
     if (QUERY) {
-        Object.keys(QUERY).forEach(key => {
-        url.searchParams.append(key, QUERY[key]);
-console.log(" url",url
-        });
+        Object.keys(QUERY.value).forEach(key => url.searchParams.append(key, QUERY[key]));
     }
 
     try {
-        console.log("Fetching data from:", url.href);
         const response = await fetch('https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?symbol=BTC', {
             method: 'GET',
             headers: headers
@@ -35,7 +25,6 @@ console.log(" url",url
         }
 
         const data = await response.json();
-        console.log("Data fetched successfully:", data);
         return data;
     } catch (error) {
         console.error('Error fetching data:', error);
