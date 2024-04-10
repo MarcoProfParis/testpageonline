7717557572397107b0d8c4d804126a595c16f1dd
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2024-04-10 12:58:10 +0200

    Update function.js

--- function.js
+++ function.js
@@ -1,4 +1,7 @@
 window.function = async function(key1,json) {
+console.log("json raw",${json.value});
+
 let json = JSON.parse(json.value);
+console.log("json parsed",${json.value});
 return json.key.value;
 };
