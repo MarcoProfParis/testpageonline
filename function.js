e1bde3a9826d462f8a688c85122640ce6da6b888
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2024-04-10 15:15:50 +0200

    Update function.js

--- function.js
+++ function.js
@@ -2,8 +2,10 @@ window.function = async function(key1,jsonstring) {
 if (jsonstring.value === undefined) return "2En attente json...";
 let json = JSON.parse(jsonstring);
 const apiUrl = 'https://cex.io/api/ticker/BTC/USD';
-
-const reponse = fetch(apiUrl);
+const requestOptions = {
+  method: 'GET'
+};
+const reponse = fetch(apiUrl,requestOptions);
 console.log("reponse",reponse);
 return json.key;
 
