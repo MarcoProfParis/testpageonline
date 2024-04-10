b350142664097657635b9a5f34e0c6f299b190c1
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2024-04-10 15:38:35 +0200

    Update function.js

--- function.js
+++ function.js
@@ -1,7 +1,7 @@
 window.function = async function(key1,jsonstring) {
-if (jsonstring.value === undefined) return "En attente json...";
+if (jsonstring.value === undefined) return "2En attente json...";
 let json = JSON.parse(jsonstring);
-console.log("url",json.url);
+console.log("key1.value ", key1.value);
 
 return key1.value;
 
