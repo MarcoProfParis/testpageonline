374fc8f433674ec07ef01f93529b30d97ecd46b0
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2024-04-10 18:22:43 +0200

    Update function.js

--- function.js
+++ function.js
@@ -1,37 +1,15 @@
 window.function = async function(key1, jsonstring) {
-    if (!jsonstring) return "En attente json..."; // Check if JSON string is provided
-    let json;
-    try {
-        json = JSON.parse(jsonstring);
-    } catch (error) {
-        console.error("Error parsing JSON:", error);
-        return "Error parsing JSON"; // Handle error accordingly
-    }
-    
+    if (jsonstring.value === undefined || jsonstring.value === '') return "En attente json...";
+    let json = JSON.parse(jsonstring.value);
     const apiUrl = json.url;
-    const requestOptions = { mode: 'no-cors' };
-    console.log("apiUrl:", apiUrl);
-    console.log("requestOptions:", requestOptions);
+    
     try {
-        let delivery = await api(apiUrl,requestOptions); // await here
-        console.log("delivery:", delivery);
-        return delivery;
+       const response = await fetch(apiUrl);
+        const text = await response.text();
+let data = JSON.parse(text);
+       return data.delivery;
     } catch (error) {
         console.error("Error:", error);
         return "Error fetching data"; // Handle error accordingly
     }
 };
-
-async function api(apiUrl,requestOptions){ // async keyword here
-    try {
-        const response = await fetch(apiUrl,requestOptions);
-        console.log("response:", response);
-        const text = await response.text();
-        console.log("text:", text);
-        let data = JSON.parse(text);
-        return data.delivery;
-    } catch (error) {
-        console.error("API Error:", error);
-        throw error; // Rethrow the error to handle it in the calling function
-    }
-}
