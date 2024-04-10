8c03038f41d804c505e6322953685fba141b274e
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2024-04-10 15:41:00 +0200

    Update function.js

--- function.js
+++ function.js
@@ -1,7 +1,7 @@
 window.function = async function(key1,jsonstring) {
-if (jsonstring.value === undefined) return "2En attente json...";
-let json = JSON.parse(jsonstring);
-console.log("key1.value ", key1.value);
+if (jsonstring.value === undefined) return "3En attente json...";
+
+console.log("Value of obj.webhook:", key1.value);
 
 return key1.value;
 
