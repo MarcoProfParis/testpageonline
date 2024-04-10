81461e8f9aca7ba8456619413b2d2c6394a8acad
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2024-04-10 16:42:05 +0200

    s

--- function.js
+++ function.js
@@ -1,21 +1,21 @@
-window.function = async function(key1,jsonstring) {
-if (jsonstring.value === undefined) return "En attente json...";
-let json = JSON.parse(jsonstring.value);
-const apiUrl = json.url;
-const requestOptions = {method: 'GET',mode: 'no-cors'};
-// const response = await fetch(apiUrl,requestOptions);
-// console.log("response:", response);
-// const data = await response.json();
-try {
-    const response = await fetch(apiUrl, requestOptions);
-    const text = await response.text(); // Get the response as text
-    console.log("Response text:", text);
-    const data = JSON.parse(text); // Parse the text as JSON
-    console.log("reponse:", data);
-    return key1.value;
-} catch (error) {
-    console.error('Error:', error);
-    throw error; // You may handle errors differently as per your requirement
-}
-
+window.function = async function(key1, jsonstring) {
+    if (jsonstring.value === undefined || jsonstring.value === '') return "En attente json...";
+    let json;
+    try {
+        json = JSON.parse(jsonstring.value);
+    } catch (error) {
+        console.error('Invalid JSON:', jsonstring.value);
+        throw new Error('Invalid JSON');
+    }
+    const apiUrl = json.url;
+    const requestOptions = {method: 'GET', mode: 'no-cors'};
+    try {
+        const response = await fetch(apiUrl, requestOptions);
+        const data = await response.json();
+        console.log("reponse:", data);
+        return key1.value;
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
+    }
 };
