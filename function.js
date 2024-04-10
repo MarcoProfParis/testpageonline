bfbbe373ea7a2f2fd5614c824fc7e5d8f52b2aa4
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2024-04-10 16:34:36 +0200

    x

--- function.js
+++ function.js
@@ -3,10 +3,17 @@ if (jsonstring.value === undefined) return "En attente json...";
 let json = JSON.parse(jsonstring.value);
 const apiUrl = json.url;
 const requestOptions = {method: 'GET',mode: 'no-cors'};
-const response = await fetch(apiUrl,requestOptions);
-console.log("reponse:", response);
-const data = await response.json();
-console.log("reponse:", data);
-return key1.value;
+// const response = await fetch(apiUrl,requestOptions);
+// console.log("response:", response);
+// const data = await response.json();
+try {
+    const response = await fetch(apiUrl, requestOptions);
+    const data = await response.json();
+    console.log("reponse:", data);
+    return key1.value;
+} catch (error) {
+    console.error('Error:', error);
+    throw error; // You may handle errors differently as per your requirement
+}
 
 };
