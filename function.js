8f95023375014e927465cb4c8aff0a438e953e77
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2024-04-10 13:25:30 +0200

    Update function.js

--- function.js
+++ function.js
@@ -1,5 +1,6 @@
 window.function = async function(key1,jsonstring) {
-if (jsonstring.value === undefined) return "5En attente json...";
+if (jsonstring.value === undefined) return "6En attente json...";
+let json = JSON.parse(jsonstring);
 const apiUrl = 'https://cex.io/api/ticker/BTC/USD';
 fetch(apiUrl)
   .then(response => {
@@ -7,6 +8,7 @@ fetch(apiUrl)
       throw new Error('Network response was not ok');
     }
    console.log("response",response.json());
+return json.key
   })
   .then(data => {
     console.log(data);
