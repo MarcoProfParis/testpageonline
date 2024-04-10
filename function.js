4af0e9a6000e835d8028637ef3899c0000df9208
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2024-04-10 15:24:51 +0200

    Update function.js

--- function.js
+++ function.js
@@ -1,15 +1,9 @@
 window.function = async function(key1,jsonstring) {
-if (jsonstring.value === undefined) return "7En attente json...";
+if (jsonstring.value === undefined) return "8En attente json...";
 let json = JSON.parse(jsonstring);
 const apiUrl = json.url;
 console.log("apiUrl",apiUrl);
-const requestOptions = {
-  method: 'GET'
-};
-console.log("requestOptions", requestOptions);
-console.log("requestOptions string",JSON.stringify(requestOptions));
-const reponse = await fetch(apiUrl,requestOptions);
-console.log("reponse",reponse);
+
 return json.key;
 
 };
