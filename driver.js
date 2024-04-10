10d0dfb7ff0d54adc42a35d3f4f3170423899b40
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2024-04-10 18:05:04 +0200

    Update driver.js

--- driver.js
+++ driver.js
@@ -1,27 +1,34 @@
 window.addEventListener("message", async function(event) {
-  const { origin, data: { key, params } } = event;
+    const { origin, data: { key1, jsonstring } } = event.data; // Extracting key1 and jsonstring from event data
 
-  let result;
-  let error;
-  try {
-    result = await window.function(...params);
-  } catch (e) {
-    result = undefined;
+    let result;
+    let error;
     try {
-      error = e.toString();
+        result = await window.function(key1, jsonstring); // Passing key1 and jsonstring to window.function
     } catch (e) {
-      error = "Exception can't be stringified.";
+        result = undefined;
+        try {
+            error = e.toString();
+        } catch (e) {
+            error = "Exception can't be stringified.";
+        }
     }
-  }
 
-  const response = { key };
-  if (result !== undefined) {
-    // FIXME: Remove `type` once that's in staging
-    response.result = { value: result };
-  }
-  if (error !== undefined) {
-    response.error = error;
-  }
+    const response = { key: key1 }; // Setting key to key1
+    if (result !== undefined) {
+        // FIXME: Remove `type` once that's in staging
+        response.result = { value: result };
+    }
+    if (error !== undefined) {
+        response.error = error;
+    }
 
-  event.source.postMessage(response, "*");
+    event.source.postMessage(response, "*");
 });
+
+function sendMessage() {
+    const key1 = document.getElementById("key1").value;
+    const jsonstring = document.getElementById("jsonstring").value;
+
+    window.parent.postMessage({ key1, jsonstring }, "*");
+}
